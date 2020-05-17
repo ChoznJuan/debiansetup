1d1ff8d31340f3fadc20d57478c2a7cba6421af9
D. Juan Gray <djg5826@gmail.com>
2020-05-17 10:26:07 -0400

    install neofetch

--- debiansetup.sh
+++ debiansetup.sh
@@ -18,7 +18,7 @@
 #greeter-hide-users=false
 
 #download apps
-sudo apt install terminator nautilus-admin bc curl dkms gedit git build-essential chromium chromium-l10n -yy
+sudo apt install terminator nautilus-admin bc curl dkms gedit git build-essential neofetch chromium chromium-l10n -yy
 
 #remove apps
 sudo apt purge mousepad xterm thunar firefox-esr -yy
