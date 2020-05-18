84b24bd4df2058d95fb999a1bb85a3ea3d146249
D. Juan Gray <djg5826@gmail.com>
2020-05-18 16:07:05 -0400

    Update debiansetup.sh

--- debiansetup.sh
+++ debiansetup.sh
@@ -21,7 +21,7 @@
 sudo apt install terminator nautilus-admin bc curl dkms gedit git build-essential neofetch chromium chromium-l10n -yy
 
 #remove apps
-sudo apt purge mousepad xterm thunar firefox-esr -yy
+sudo apt purge mousepad xfce4-terminal xterm thunar firefox-esr -yy
 
 #remove firefox cache
 if [ -d "/home/$USER/.mozilla" ]; then
